769e12732927f2a5aba3022f2d8d494a07c313a7
catsTest <Hawk@12345>
2020-06-07 22:06:32 +0300

    correct

--- src/test/java/CatsTest.java
+++ src/test/java/CatsTest.java
@@ -23,111 +23,99 @@ public class CatsTest {
     }
 
     @Test
-    public void testKnowledge() throws InterruptedException {
-        Thread.sleep(185463);
-//        MainPage mainPage = new MainPage();
-//        Assert.assertTrue(mainPage.isMainPage(), "U're reached not main page");
-//        mainPage.goToKnowledgeTesting();
-//        mainPage.clickGoToTest();
-//        KnowledgeTestingPage knowledgePage = new KnowledgeTestingPage();
-//        Assert.assertTrue(knowledgePage.isKnowledgePage(), "U're reached not knowledge testing page. Check locator pls.");
+    public void testKnowledge() {
+        MainPage mainPage = new MainPage();
+        mainPage.goToKnowledgeTesting();
+        mainPage.clickGoToTest();
+        KnowledgeTestingPage knowledgePage = new KnowledgeTestingPage();
+
+        knowledgePage.createTestClick();
+        CreateEditTestPage createPage = new CreateEditTestPage();
+        createPage.fillPageAndClick();
+//        Assert.assertTrue(knowledgePage.isTestCreated(), "Error with create test.");
+
+        //edit Test
+        knowledgePage.findTest();
+        CreateEditTestPage editTestPage = new CreateEditTestPage();
+        editTestPage.EditTestAndSave();
+//        Assert.assertTrue(knowledgePage.isEditedTest(), "Error with edit test.");
+
+//        //questions
+        knowledgePage.goToQuestions();
+        QuestionsPage questionsPage = new QuestionsPage();
+//        Assert.assertTrue(questionsPage.isQuestionPage(), "Error, u're reached not question page");
+
+        //add question from another test
+        questionsPage.addQuestionFromClick();
+        AddQuestionFromAnotherPage addPage = new AddQuestionFromAnotherPage();
+        String questionName = addPage.selectAll();
+
+        //edit question
+        questionsPage.editQuestion(questionName);
+        CreateEditQuestionPage createEditQuestionPage = new CreateEditQuestionPage();
+        createEditQuestionPage.editQuestionAndSave();
+
+        //delete question
+        questionsPage.deleteQuestion();
+        DeletePage deletePage = new DeletePage();
+        deletePage.clickDelete();
+
+        //add question
+        questionsPage.addQuestion();
+        createEditQuestionPage.fillQuestionName();
+        createEditQuestionPage.fillDescription();
+        createEditQuestionPage.fillAnswer();
+        createEditQuestionPage.clickSave();
+
+        questionsPage.backToTest();
+        knowledgePage.startTest();
+
+        PassTestPage passTestPage = new PassTestPage();
+        passTestPage.clickNext();
+        passTestPage.answerOnQuestions();
+        passTestPage.backToTest();
+
+        //search task
+        knowledgePage.fillSearchString();
 //
-//        knowledgePage.createTestClick();
-//        CreateEditTestPage createPage = new CreateEditTestPage();
-//        Assert.assertTrue(createPage.isFillTestPageOpen(), "U're reached not create test page. Pls check locator");
-//        createPage.fillPageAndClick();
-////        Assert.assertTrue(knowledgePage.isTestCreated(), "Error with create test.");
-//
-//        //edit Test
-//        knowledgePage.findTest();
-//        CreateEditTestPage editTestPage = new CreateEditTestPage();
-//        editTestPage.EditTestAndSave();
-////        Assert.assertTrue(knowledgePage.isEditedTest(), "Error with edit test.");
-//
-////        //questions
-//        knowledgePage.goToQuestions();
-//        QuestionsPage questionsPage = new QuestionsPage();
-////        Assert.assertTrue(questionsPage.isQuestionPage(), "Error, u're reached not question page");
-//
-//        //add question from another test
-//        questionsPage.addQuestionFromClick();
-//        AddQuestionFromAnotherPage addPage = new AddQuestionFromAnotherPage();
-//        String questionName = addPage.selectAll();
-//
-//        //edit question
-//        questionsPage.editQuestion(questionName);
-//        CreateEditQuestionPage createEditQuestionPage = new CreateEditQuestionPage();
-//        createEditQuestionPage.editQuestionAndSave();
-//
-//        //delete question
-//        questionsPage.deleteQuestion();
-//        DeletePage deletePage = new DeletePage();
-//        deletePage.clickDelete();
-//
-//        //add question
-//        questionsPage.addQuestion();
-//        Assert.assertTrue(createEditQuestionPage.isCreateEditPage(), "Error. u're reached not create page");
-//        createEditQuestionPage.fillQuestionName();
-//        createEditQuestionPage.fillDescription();
-//        createEditQuestionPage.fillAnswer();
-//        createEditQuestionPage.clickSave();
-//
-//        questionsPage.backToTest();
-//        knowledgePage.startTest();
-//
-//        PassTestPage passTestPage = new PassTestPage();
-//        passTestPage.clickNext();
-//        passTestPage.answerOnQuestions();
-//        passTestPage.backToTest();
-//
-//        //search task
-//        knowledgePage.fillSearchString();
-//        Assert.assertTrue(knowledgePage.isTestFindAndClear(), "Error. test not found! pls check locators and logic.");
-////
-//        //delete task
-//        knowledgePage.deleteTest();
-//        deletePage.clickDelete();
-//
-//        knowledgePage.goToMain();
-//
-//        mainPage.logOut();
+        //delete task
+        knowledgePage.deleteTest();
+        deletePage.clickDelete();
+
+        knowledgePage.goToMain();
+
+        mainPage.logOut();
     }
 
     @Test
-    public void testEUMK() throws InterruptedException {
-        Thread.sleep(50323);
-//        MainPage mainPage = new MainPage();
-//        Assert.assertTrue(mainPage.isMainPage(), "U're reached not main page");
-//        mainPage.goToEUMK();
-//        mainPage.clickToEumk();
-//
-//        EumkPage eumkPage = new EumkPage();
-//        Assert.assertTrue(eumkPage.isOurPage(), " U're reached not eumk page! Pls check ur locator");
-//
-//        //add
-//        eumkPage.addBookClick();
-//        AddEditBookPage addEditBookPage = new AddEditBookPage();
-//        Assert.assertTrue(addEditBookPage.isAddBookPage(), "U're reached not add dialog. pls, check u'r url");
-//        addEditBookPage.fillInfoAndClickCreate();
-//        //add eumk, edit eumk, delete eumk, add
-//
-//        //edit
-//        eumkPage.isBookCreated();
-//        eumkPage.selectBookAndClickEdit();
-//        addEditBookPage.isEditDialogOpen();
-//        addEditBookPage.editBookAndSave();
-//        eumkPage.isBookEdit();
-//
-//        //delete
-//        eumkPage.selectBookAndDelete();
-//        DeletePage deletePage = new DeletePage();
-//        deletePage.isDeletePage();
-//        deletePage.clickDelete();
+    public void testEUMK() {
+        MainPage mainPage = new MainPage();
+        mainPage.goToEUMK();
+        mainPage.clickToEumk();
 
+        EumkPage eumkPage = new EumkPage();
+
+        //add
+        eumkPage.addBookClick();
+        AddEditBookPage addEditBookPage = new AddEditBookPage();
+        addEditBookPage.fillInfoAndClickCreate();
+
+        //edit
+        eumkPage.isBookCreated();
+        eumkPage.selectBookAndClickEdit();
+        addEditBookPage.isEditDialogOpen();
+        addEditBookPage.editBookAndSave();
+        eumkPage.isBookEdit();
+
+        //delete
+        eumkPage.selectBookAndDelete();
+        DeletePage deletePage = new DeletePage();
+        deletePage.isDeletePage();
+        deletePage.clickDelete();
     }
 
-//    @AfterTest
-//    public void after() {
-//        Browser.quit();
-//    }
+    @AfterTest
+    public void after() {
+        Browser.quit();
+    }
 }
